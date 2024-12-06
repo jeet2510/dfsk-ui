c4f1f6588230fd2105af29497f769ab0bce400d0
Prinkesh <prinkesh.patel@smbsolutionkwt.com>
2024-12-06 11:50:39 +0530

    html design

--- resources/views/website/templates/services.blade.php
+++ resources/views/website/templates/services.blade.php
@@ -111,7 +111,7 @@
         <div class="accordion-item">
             <h2 class="accordion-header" id="map-headingOne">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#map-collapseOne" aria-expanded="false" aria-controls="map-collapseOne">
-                    DFSK Deira
+                    Green Motors Khalidiya
                 </button>
             </h2>
             <div id="map-collapseOne" class="accordion-collapse collapse" aria-labelledby="map-headingOne" data-bs-parent="#mapaccordion">
@@ -119,14 +119,9 @@
                     <div class="map-address-box">
                         <div class="mb-4">
                             <p>
-                                <b>Address:</b> 22A St - Al ittihad Road, Deira, Dubai.
+                                <b>Address:</b> Hili Tower B, Mubarak Bin Mohamed St. Al Khalidiyah, AUH
                             </p>
-                            <p>
-                                <b>Opening Date:</b> 14 November 2022
-                            </p>
-                            <p>
-                                <b>Opening Hours:</b> Monday to Sunday - 09:00–21:00
-                            </p>
+
                         </div>
                         <div class="ma-contact-box mb-4">
                             <h6>Contact:</h6>
@@ -135,7 +130,7 @@
                                     <path d="M21.4902 14.9492L17.0901 12.9775L17.0779 12.9719C16.8495 12.8742 16.6003 12.835 16.3529 12.8579C16.1055 12.8807 15.8677 12.9648 15.6611 13.1027C15.6367 13.1188 15.6133 13.1362 15.591 13.155L13.3176 15.0931C11.8774 14.3935 10.3904 12.9178 9.69087 11.4962L11.6317 9.18827C11.6504 9.16492 11.6682 9.14157 11.685 9.11635C11.8199 8.91022 11.9017 8.67393 11.9232 8.42852C11.9447 8.18311 11.9052 7.93619 11.8083 7.70974V7.69853L9.83097 3.29094C9.70277 2.99511 9.48233 2.74867 9.20256 2.58842C8.92279 2.42816 8.59869 2.36269 8.27865 2.40177C7.01304 2.56831 5.85133 3.18985 5.01049 4.15032C4.16966 5.11079 3.70719 6.34449 3.70948 7.62101C3.70948 15.037 9.74318 21.0707 17.1592 21.0707C18.4357 21.073 19.6694 20.6106 20.6299 19.7697C21.5904 18.9289 22.2119 17.7672 22.3784 16.5016C22.4176 16.1816 22.3523 15.8576 22.1922 15.5779C22.0321 15.2981 21.7859 15.0776 21.4902 14.9492ZM17.1592 19.5763C13.9895 19.5729 10.9507 18.3122 8.70935 16.0709C6.46804 13.8296 5.20736 10.7907 5.20389 7.62101C5.20038 6.70894 5.52898 5.82677 6.12832 5.13926C6.72766 4.45174 7.55676 4.00589 8.46078 3.88497C8.46041 3.8887 8.46041 3.89245 8.46078 3.89618L10.4222 8.28602L8.4916 10.5968C8.47201 10.6193 8.45421 10.6433 8.43837 10.6687C8.29781 10.8844 8.21536 11.1327 8.19899 11.3896C8.18263 11.6465 8.23291 11.9033 8.34496 12.1351C9.19118 13.8658 10.935 15.5965 12.6844 16.4418C12.9178 16.5528 13.1761 16.6013 13.4339 16.5827C13.6918 16.564 13.9404 16.4788 14.1554 16.3353C14.1794 16.3191 14.2025 16.3017 14.2245 16.283L16.4951 14.3459L20.885 16.312C20.885 16.312 20.8924 16.312 20.8952 16.312C20.7758 17.2173 20.3306 18.048 19.643 18.6488C18.9553 19.2497 18.0723 19.5794 17.1592 19.5763Z" fill="white"/>
                                 </svg>
 
-                                <a class="text-white" href="tel:04 255 6392">04 255 6392</a>
+                                <a class="text-white" href="tel:04 255 6392">02 553 0362</a>
                             </p>
                             <p>
                                 <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -155,7 +150,7 @@
         <div class="accordion-item">
             <h2 class="accordion-header" id="map-headingTwo">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#map-collapseTwo" aria-expanded="false" aria-controls="map-collapseTwo">
-                    DFSK Khalidiya
+                    Green Motors Musaffah
                 </button>
             </h2>
             <div id="map-collapseTwo" class="accordion-collapse collapse" aria-labelledby="map-headingTwo" data-bs-parent="#mapaccordion">
@@ -199,7 +194,8 @@
         <div class="accordion-item">
             <h2 class="accordion-header" id="map-headingThree">
                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#map-collapseThree" aria-expanded="false" aria-controls="map-collapseThree">
-                    DFSK Musaffah
+                    Green Motors Abu Ras Al
+                    Khaimah
                 </button>
             </h2>
             <div id="map-collapseThree" class="accordion-collapse collapse" aria-labelledby="map-headingThree" data-bs-parent="#mapaccordion">
